002850707c86db6ce04df892a86fc9ebdf9993bf
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-24 16:23:09 +0700

    Initial commit

---
+++ QuizXIB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizXIB
+//
+//  Created by Supakit Thanadittagorn on 8/24/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizXIB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizXIB
+//
+//  Created by Supakit Thanadittagorn on 8/24/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
